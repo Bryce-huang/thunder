30c699e205b06c00d4c3e01f83332cb207b951b8
bryce huang <cherbini@qq.com>
2019-08-12 00:13:34 +0800

    添加docker部署文件以及脚本

--- scripts/CI.sh
+++ scripts/CI.sh
@@ -1,7 +1,24 @@
+#安装jenkins
 chown -R 1000:1000 /home/jenkins
-
 docker run -it -u root -d -p 8000:8080 -p 50000:50000 --name jenkins \
 -v /home/jenkins:/var/jenkins_home -e JAVA_OPTS=-Duser.timezone=Asia/Shanghai \
 -v $(which docker):/usr/bin/docker -v /var/run/docker.sock:/var/run/docker.sock \
 -v  /home/jenkins/apache-maven-3.5.0:/var/maven_home \
 --privileged=true jenkins:2.150
+
+# 运行eureka
+docker run -p 8761:8761 \
+--env JAVA_OPTS='-server -Xmx512m' \
+--env PROFILE='dev' \
+--env EUREKA_PORT='8761' \
+--name single-eureka -d \
+--restart always brycehuang/single-eureka:0.0.1-SNAPSHOT \
+--network=host
+
+
+# 运行config
+docker run -p 8888:8888 \
+--env JAVA_OPTS='-server -Xmx512m' \
+--name single-config -d \
+--restart always brycehuang/config:1.0-SNAPSHOT \
+--network=host
--- single-eureka/src/main/java/fun/bryce/singleeureka/SingleEurekaApplication.java
+++ single-eureka/src/main/java/fun/bryce/singleeureka/SingleEurekaApplication.java
@@ -1,24 +1,15 @@
 package fun.bryce.singleeureka;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * @author bryce
  */
 @SpringBootApplication
 @EnableEurekaServer
-@RestController
-@RequestMapping("/demo")
 @EnableSwagger2Doc
 public class SingleEurekaApplication
 {
@@ -26,15 +17,6 @@ public class SingleEurekaApplication
     public static void main(String[] args)
     {
         SpringApplication.run(SingleEurekaApplication.class, args);
-    }
-
-
-    @ApiOperation(value = "demo服务的get方法")
-    @ApiParam(value = "id参数描述", required = true)
-    @RequestMapping(value = "{id}", method = RequestMethod.GET)
-    public String get(@PathVariable Integer id)
-    {
-        return "得到你的id:" + id;
     }
 
 }
